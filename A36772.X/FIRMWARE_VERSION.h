cfebfcc361f28145625fab06eea4138aca48046a
hwanetick <hwanetick@etm-inc.com>
2016-05-26 19:18:42 -0700

    A36772 Rev 1.2.1
    Updated firmware version

--- A36772.X/FIRMWARE_VERSION.h
+++ A36772.X/FIRMWARE_VERSION.h
@@ -2,8 +2,8 @@
 #define __FIRMWARE_VERISON_H
 
 #define FIRMWARE_AGILE_REV     1
-#define FIRMWARE_BRANCH        103
-#define FIRMWARE_MINOR_REV     6
+#define FIRMWARE_BRANCH        2
+#define FIRMWARE_MINOR_REV     1
 
 #endif
 
